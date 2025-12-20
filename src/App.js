8f8b229ceb46f0e651c9e86ab5ce5a593f59a941
Visha Raj <visha@example.com>
2025-12-20 22:08:54 +0530

    Update address to just Bangalore

--- src/App.js
+++ src/App.js
@@ -969,6 +969,7 @@ const App = () => {
                 </p>
               </div>
               <div className="md:w-1/3 text-center md:text-right">
+                <p className={`font-medium ${isDarkMode ? 'text-gray-200' : 'text-gray-800'}`}>August 10, 2024 | Bengaluru, India</p>
                 <button
                   onClick={() => openInfoModal('In-Person Workshop: Advanced Team Building', 'Registration for this in-person workshop is opening soon! Elevate your team’s collaboration and problem-solving skills.')}
                   className={`mt-3 py-2 px-4 rounded-full transition duration-300 ${isDarkMode ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-indigo-600 text-white hover:bg-indigo-700'}`}>Register</button>
@@ -1250,7 +1251,7 @@ const App = () => {
                 </p>
                 <p className={`flex items-center ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>
                   <svg className={`w-5 h-5 mr-3 ${isDarkMode ? 'text-indigo-300' : 'text-indigo-600'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path></svg>
-                  Provident Wellworth City Dodhballapur Main Road Bangalore:561-203
+                   Bangalore
                 </p>
                 {/* LinkedIn Icon and Link */}
                 <p className={`flex items-center ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>
